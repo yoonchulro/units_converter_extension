c27ae0926eeb93c0feb6ae184f53fbd6420254e7
3pidemix <3pidemix@gmail.com>
2020-10-04 16:00:28 -0400

    Adds Length.test.js

---
+++ test/Length.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert')
+var chai = require('chai')
+var expect = chai.expect
+var should = chai.should()
+
+const Length = require('../code/conversions/Length')
+
+describe('Length', function() {
+    it('Conversions should not be empty', function() {
+        let inches = new Length("inch", ["meters", "miles", "kilometers", "centimeters", "millimeters", "yard", "feet", "furlong", "chain"]);
+        assert.notStrictEqual(inches.getAllConversions(49), "");
+    })
+})
