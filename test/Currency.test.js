d8421fcfe54170494d740b66204e5e726d98530c
Amitha Raghava Raju <araghav4@wolftech.ad.ncsu.edu>
2020-10-10 21:02:11 -0400

    Chrome extension and test cases verified

--- test/Currency.test.js
+++ test/Currency.test.js
@@ -1,13 +1,35 @@
-var assert = require('assert')
-var chai = require('chai')
-var expect = chai.expect
-var should = chai.should()
+var assert = require("assert");
+var chai = require("chai");
+var expect = chai.expect;
+var should = chai.should();
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
 
-const Currency = require('../code/conversions/Currency')
+const Currency = require("../code/conversions/Currency");
 
-describe('Currency', function() {
-    it('Conversions should not be empty', function() {
-        let usd = new Currency("USD", ["eur", "gbp", "inr", "jpy", "cad","aud","chf"]);
-        assert.notStrictEqual(usd.getAllConversions(49.99), "");
-    })
-})
+describe("Currency", function () {
+  let usd = new Currency("USD", [
+    "eur",
+    "gbp",
+    "inr",
+    "jpy",
+    "cad",
+    "aud",
+    "chf",
+  ]);
+  it("Conversions should not be empty", function () {
+    assert.notStrictEqual(usd.getAllConversions(49.99, 1), "");
+  });
+
+  it("Standard Conversions should not be empty", function () {
+    assert.notStrictEqual(usd.getStandardConversion(50), "");
+  });
+
+  it("USD Conversion check", function () {
+    return expect(
+      usd.getAllConversions(50, 1).then(function (data) {
+        return { currency: data.substring(1) };
+      })
+    ).to.eventually.haveOwnProperty("currency");
+  });
+});
