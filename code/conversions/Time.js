22a02a7447ef773d00b7ecc188b448c941d2828f
shubhambalsaraf <65739112+shubhambalsaraf@users.noreply.github.com>
2020-10-16 17:39:52 -0400

    unit test for time.js

--- code/conversions/Time.js
+++ code/conversions/Time.js
@@ -3,36 +3,60 @@
  */
 
 class Time {
-	/**
-	 * 
-	 * @param {Object} unit Time unit
-	 * @param {Array} arr Array of all conversions
-	 */
+    /**
+     *
+     * @param {Object} unit Time unit
+     * @param {Array} arr Array of all conversions
+     */
 
-	constructor(unit, arr){
-		this.unit = unit;
-		this.arr = arr;
-	}
+    constructor(unit, arr) {
+        this.unit = unit;
+        this.arr = arr;
+    }
 
-	/**
-	 * Our standard conversion is seconds, so we try to convert all selection to seconds
-	 * @property {Function} getStandardConversion Units converted into Seconds
-	 * @param {Object} quantity user query unit
-	 * @returns Value in Seconds
-	 */
-	getStandardConversion(quantity) {
-		
-		switch(this.unit.toLowerCase()){
-			case "seconds": return quantity;
-			case "minutes": return quantity * 60;
-			case "hours": return quantity * (60 * 60);
-			case "days": return quantity * (24 * 60 * 60);
-			case "weeks": return quantity * (7 * 24 * 60 * 60);
-			default: return null;
-		}
-	}
-	
-	/**
+    getPrecision(number) {
+        let parts = number.toString().split('.');
+        if (parts.length <= 1) {
+            return number < 0 ? parts[0].length - 1 : parts[0].length;
+        }
+        let intlen = number < 0 ? parts[0].length - 1 : parts[0].length;
+        return intlen + parts[1].length;
+    }
+
+    /**
+     * Return number with a max precison of 10
+     * @param {number} number value to convert
+     * @param {number} precision upto number precision
+     * @returns precise value with precision of upto 10
+     */
+    getPreciseNumber(number, precision) {
+        return number.toPrecision(Math.min(Math.max(this.getPrecision(number), precision), 10));
+    }
+
+    /**
+     * Our standard conversion is seconds, so we try to convert all selection to seconds
+     * @property {Function} getStandardConversion Units converted into Seconds
+     * @param {Object} quantity user query unit
+     * @returns Value in Seconds
+     */
+    getStandardConversion(quantity) {
+        switch (this.unit.toLowerCase()) {
+            case 'seconds':
+                return quantity;
+            case 'minutes':
+                return quantity * 60;
+            case 'hours':
+                return quantity * (60 * 60);
+            case 'days':
+                return quantity * (24 * 60 * 60);
+            case 'weeks':
+                return quantity * (7 * 24 * 60 * 60);
+            default:
+                return null;
+        }
+    }
+
+    /**
 	 * From our standard conversion we try to convert into all the other units specified in arr property of this class
 	with a precision no more than 10
 	 * @param {Object} quantity value to convert
@@ -40,38 +64,37 @@ class Time {
 	 * @returns precise value with precsion of upto 10
 	 */
 
-	getAllConversions(quantity, precision){
-		let res = "";
-		this.arr.forEach((u) => {
-			switch(u.toLowerCase()){
-				case "seconds": {
-					res += "," + getPreciseNumber(quantity, precision) + " sec";
-					break;
-				}
-				case "minutes": {
-					let conv = quantity / 60;
-					res += ',' + getPreciseNumber(conv, precision) + " mins";
-					break;
-				}
-				case "hours": {
-					let conv = quantity / (60 * 60);
-					res += ',' + getPreciseNumber(conv, precision) + " hours";
-					break;
-				}
-				case "days": {
-					let conv = quantity / (24 * 60 * 60);
-					res += ',' + getPreciseNumber(conv, precision) + " days";	
-					break;
-				}
-				case "weeks": {
-					let conv = quantity / (7 * 24 * 60 * 60);
-					res += ',' + getPreciseNumber(conv, precision) + " days";	
-					break;
-				}
-			}
-
-		});
-		return res;
-	}
+    getAllConversions(quantity, precision) {
+        let res = '';
+        this.arr.forEach(u => {
+            switch (u.toLowerCase()) {
+                case 'seconds': {
+                    res += ',' + this.getPreciseNumber(quantity, precision) + ' sec';
+                    break;
+                }
+                case 'minutes': {
+                    let conv = quantity / 60;
+                    res += ',' + this.getPreciseNumber(conv, precision) + ' mins';
+                    break;
+                }
+                case 'hours': {
+                    let conv = quantity / (60 * 60);
+                    res += ',' + this.getPreciseNumber(conv, precision) + ' hours';
+                    break;
+                }
+                case 'days': {
+                    let conv = quantity / (24 * 60 * 60);
+                    res += ',' + this.getPreciseNumber(conv, precision) + ' days';
+                    break;
+                }
+                case 'weeks': {
+                    let conv = quantity / (7 * 24 * 60 * 60);
+                    res += ',' + this.getPreciseNumber(conv, precision) + ' days';
+                    break;
+                }
+            }
+        });
+        return res;
+    }
 }
 module.exports = Time;
