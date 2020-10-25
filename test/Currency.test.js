d17c1092b8f26166524a94ec1b6fdf7373769b5a
Shubham Balsaraf <65739112+shubhambalsaraf@users.noreply.github.com>
2020-10-25 19:17:28 -0400

    Update Currency.test.js
    - new currency update

--- test/Currency.test.js
+++ test/Currency.test.js
@@ -16,14 +16,24 @@ describe('Currency', function() {
     var curCad;
     var curAud;
     var curChf;
-    curUsd = new Currency('usd', ['eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf']);
-    curEur = new Currency('eur', ['usd', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf']);
-    curGbp = new Currency('gbp', ['usd', 'eur', 'inr', 'jpy', 'cad', 'aud', 'chf']);
-    curInr = new Currency('inr', ['usd', 'eur', 'gbp', 'jpy', 'cad', 'aud', 'chf']);
-    curJpy = new Currency('jpy', ['usd', 'eur', 'gbp', 'inr', 'cad', 'aud', 'chf']);
-    curCad = new Currency('cad', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'aud', 'chf']);
-    curAud = new Currency('aud', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'chf']);
-    curChf = new Currency('chf', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud']);
+    var curHkd;
+    var curPhp;
+    var curCzk;
+    var curMxn;
+    var curBrl;
+    curUsd = new Currency('usd', ['eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curEur = new Currency('eur', ['usd', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curGbp = new Currency('gbp', ['usd', 'eur', 'inr', 'jpy', 'cad', 'aud', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curInr = new Currency('inr', ['usd', 'eur', 'gbp', 'jpy', 'cad', 'aud', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curJpy = new Currency('jpy', ['usd', 'eur', 'gbp', 'inr', 'cad', 'aud', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curCad = new Currency('cad', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'aud', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curAud = new Currency('aud', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'chf', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curChf = new Currency('chf', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'hkd','php', 'czk', 'mxn', 'brl']);
+    curHkd = new Currency('hkd', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf','php', 'czk', 'mxn', 'brl']);
+    curPhp = new Currency('php', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf','hkd', 'czk', 'mxn', 'brl']);
+    curCzk = new Currency('czk', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf','hkd','php', 'mxn', 'brl']);
+    curMxn = new Currency('mxn', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf','hkd','php', 'czk', 'brl']);
+    curBrl = new Currency('brl', ['usd', 'eur', 'gbp', 'inr', 'jpy', 'cad', 'aud', 'chf','hkd','php', 'czk', 'mxn']);
 
     it('Conversions should not be empty', function() {
         assert.notStrictEqual(curUsd.getAllConversions(49.99, 1), '');
@@ -34,6 +44,11 @@ describe('Currency', function() {
         assert.notStrictEqual(curCad.getAllConversions(49.99, 1), '');
         assert.notStrictEqual(curAud.getAllConversions(49.99, 1), '');
         assert.notStrictEqual(curChf.getAllConversions(49.99, 1), '');
+        assert.notStrictEqual(curHkd.getAllConversions(49.99, 1), '');
+        assert.notStrictEqual(curPhp.getAllConversions(49.99, 1), '');
+        assert.notStrictEqual(curCzk.getAllConversions(49.99, 1), '');
+        assert.notStrictEqual(curMxn.getAllConversions(49.99, 1), '');
+        assert.notStrictEqual(curBrl.getAllConversions(49.99, 1), '');
     });
 
     it('Standard Conversions should not be empty', function() {
@@ -45,6 +60,11 @@ describe('Currency', function() {
         assert.notStrictEqual(curCad.getStandardConversion(50), '');
         assert.notStrictEqual(curAud.getStandardConversion(50), '');
         assert.notStrictEqual(curChf.getStandardConversion(50), '');
+        assert.notStrictEqual(curHkd.getStandardConversion(50), '');
+        assert.notStrictEqual(curPhp.getStandardConversion(50), '');
+        assert.notStrictEqual(curCzk.getStandardConversion(50), '');
+        assert.notStrictEqual(curMxn.getStandardConversion(50), '');
+        assert.notStrictEqual(curBrl.getStandardConversion(50), '');
     });
 
     it('USD Conversion check', function() {
